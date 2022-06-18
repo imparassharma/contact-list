6d6f5e6b230202d9fcf2a0aa5f345d35b10dc159
Paras Sharma <parasangiras121@gmail.com>
2022-06-18 16:09:45 +0530

    mapping

--- src/components/contacts.jsx
+++ src/components/contacts.jsx
@@ -1,18 +1,24 @@
 const contacts = [
     {
+        id:"1",
         name:"Akshay Kumar",
         phone: "+123 456 789",
-        email:"ak@gmail.com"
+        email:"ak@gmail.com",
+        img:require("../images/ak.jpg")
     },
     {
+        id:"2",
         name:"Salman Khan",
         phone: "+101 426 282",
-        email:"Sk@gmail.com"
+        email:"Sk@gmail.com",
+        img:require("../images/Sk.jpg")
     },
     {
+        id:"3",
         name:"Shah Rukh Khan",
         phone: "+987 765 432",
-        email:"SRK@gmail.com"
+        email:"SRK@gmail.com",
+        img:require("../images/SRK.jpg")
     }
 ]
 
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,31 +1,22 @@
 import React from "react";
 import Card from "./card";
 import contacts from "./contacts";
-import pic1 from "../images/ak.jpg";
-import pic2 from "../images/Sk.jpg";
-import pic3 from "../images/SRK.jpg";
+
+function create_card(contact){
+    return(
+        <Card
+        name={contact.name}
+        tel={contact.phone}
+        email={contact.email}
+        img={contact.img}
+        />
+    )
+}
 function Main(){
     return(
         <div className="Main">
-        <h1>CONTACT LIST</h1>
-        <Card
-            name={contacts[0].name}
-            img={pic1}
-            tel={contacts[0].phone}
-            email={contacts[0].email}
-        />
-        <Card
-            name={contacts[1].name}
-            img={pic2}
-            tel={contacts[1].phone}
-            email={contacts[1].email}
-        />
-        <Card
-            name={contacts[2].name}
-            img={pic3}
-            tel={contacts[2].phone}
-            email={contacts[2].email}
-        />
+            <h1>CONTACT LIST</h1>
+            {contacts.map(create_card)}
         </div>
     )
 }
